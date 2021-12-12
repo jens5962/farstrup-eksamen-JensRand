9ab8dc6009e0a77b9baee8ea20cda72cc80b7923
jens5962 <jensrand1999@gmail.com>
2021-12-12 15:39:24 +0100

    help page with link button

--- index.php
+++ index.php
@@ -1,18 +1,34 @@
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-	<title>Document</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Farstrup Produkter</title>
 </head>
 <body>
-	<!-- Hoved tekst -->
+	<!-- Produkt navn -->
 	<h1>"Produkt navn"</h1>
 
 	<!-- Info/specifikationer -->
 	<p> <strong>Information</strong> </p>
 	<p> "Produkt navn" er lavet i... </p>
 	<p> <strong>Specifikationer</strong> </p>
-	<p> Mål: xx cm høj... </p>
-	<p> Mål: xx cm bred... </p>
-	<p> Mål: xx cm lang... </p>
+	
+	<!-- Mål liste -->
+	<p> Mål: </p>
+	<ul> 
+		<li> x cm høj </li>
+		<li> x cm lang </li>
+		<li> x cm bred </li>
+	</ul>
+
+	<!-- Hjælp -->
+	<p> <strong>Hjælp</strong> </p>
+	<p> Tryk <a href=<?php echo "help.php"?>>her </a> for hjælp til vores produkter. </p>
+
+	<!-- Sikkerhed -->
+	<form></form> 	
+
 </body>
 </html>
